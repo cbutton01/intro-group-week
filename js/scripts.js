33bac709964d0625e2c72f3c146c872db740b34b
Cameron Button, Jared Lutz and Matt Nardoni <jared@>
2018-04-09 11:10:48 -0700

    add dice roll function

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function DiceRoll(sides){
+  roll = this.roll;
+  roll  =  Math.floor(parseInt(Math.random() * (sides +1)));
+  if(this.roll === 0){
+    return DiceRoll(sides);
+  }
+  console.log(roll);
+}
